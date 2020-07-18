c85790e938f7624944f6ff42eeca47ad63ad919b
IshraqKabir <digontoi99@gmail.com>
2020-07-18 12:03:19 +0600

    changed a little styling

--- resources/js/url.js
+++ resources/js/url.js
@@ -1,2 +1,4 @@
-export const url = `http://dummy-freelancer.herokuapp.com`;
+// export const url = `http://dummy-freelancer.herokuapp.com`;
 // export const url = `/`;
+
+export const url = 'http://localhost:8000'
