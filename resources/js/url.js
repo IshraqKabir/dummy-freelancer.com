959729eeae06a89091bf7c1480b81fe6e6aac629
IshraqKabir <digontoi99@gmail.com>
2020-07-18 13:18:50 +0600

    heroku

--- resources/js/url.js
+++ resources/js/url.js
@@ -1,4 +1,4 @@
-export const url = `http://dummy-freelancer.herokuapp.com`;
+export const url = `https://dummy-freelancer.herokuapp.com`;
 // export const url = `/`;
 
 // export const url = 'http://localhost:8000'
