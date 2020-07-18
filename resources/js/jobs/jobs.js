77790cdf9bbb6edcb78cf643f4b8b66aa50d697f
IshraqKabir <digontoi99@gmail.com>
2020-07-18 10:08:40 +0600

    heroku

--- resources/js/jobs/jobs.js
+++ resources/js/jobs/jobs.js
@@ -25,7 +25,7 @@ class Jobs extends React.Component
   componentDidMount ()
   {
     this.props.connect('Jobs');
-    axios.get(`${url}?q=`)
+    axios.get(`${url}/jobsapi?q=`)
     .then(response => {
       this.props.setSearchResults(response.data);
       this.props.paginate();
