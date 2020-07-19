7b5e269c6bb9c9736f753d02bc9abd4f81bf4091
IshraqKabir <digontoi99@gmail.com>
2020-07-19 12:50:49 +0600

    let's see

--- public/js/project/post-project.js
+++ public/js/project/post-project.js
@@ -37140,8 +37140,7 @@ function reducer() {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "url", function() { return url; });
-var url = "https://dummy-freelancer.herokuapp.com"; // export const url = `/`;
-// export const url = 'http://localhost:8000';
+var url = "https://dummy-freelancer.herokuapp.com";
 
 /***/ }),
 
