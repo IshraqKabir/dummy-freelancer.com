2bba3400727abd90275019886ff30d0226efa85b
IshraqKabir <digontoi99@gmail.com>
2020-07-19 15:38:41 +0600

    heroku

--- public/js/project/post-project.js
+++ public/js/project/post-project.js
@@ -37259,7 +37259,7 @@ function reducer() {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "url", function() { return url; });
-var url = 'http://localhost:8000';
+var url = "https://dummy-freelancer.herokuapp.com";
 
 /***/ }),
 
--- resources/js/url.js
+++ resources/js/url.js
@@ -1,2 +1,1 @@
-export const url = 'http://localhost:8000';
-
+export const url = `https://dummy-freelancer.herokuapp.com`;
