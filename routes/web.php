95d4be92443555abbf7c5cb69cda6ea5492cf90e
IshraqKabir <digontoi99@gmail.com>
2020-07-20 08:45:37 +0600

    ForceSSL middleware implemented

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Resources\Skill as SkillResource;
 use App\Skill;
 
-if (env('APP_ENV') === 'production') {
-    \URL::forceScheme('https');
-} else {
-    \URL::forceScheme('http');
-}
-
-
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
